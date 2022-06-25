9881a0553784a1dabdcd7a8aeac63301c631f5fe
Maksym Bilan <>
2022-06-25 20:12:45 +0300

    Refactoring & comments

--- Sources/SwiftIPConfig/SwiftIPConfig.swift
+++ Sources/SwiftIPConfig/SwiftIPConfig.swift
@@ -3,16 +3,9 @@ import gateway
 
 public enum SwiftIPConfig {
 
-    public static func getGatewayIP() -> String? {
-        var gatewayaddr = in_addr()
-        let result = get_gateway(&gatewayaddr.s_addr)
-        guard result >= 0 else {
-            return nil
-        }
-        return String(cString: inet_ntoa(gatewayaddr))
-    }
-
-    public static func getIP() -> String? {
+    /// Returns local IP address. For example: "192.168.1.34"
+    /// - Returns: String or nil
+    public static func getIP(ethernet: String = "en0") -> String? {
         var address: String?
         var ifaddr: UnsafeMutablePointer<ifaddrs>?
 
@@ -34,7 +27,7 @@ public enum SwiftIPConfig {
                     return nil
                 }
                 let name: String = String(cString: ifa_name)
-                if name == "en0" {
+                if name == ethernet {
                     var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
                     getnameinfo(interface.ifa_addr, socklen_t((interface.ifa_addr.pointee.sa_len)), &hostname, socklen_t(hostname.count), nil, socklen_t(0), NI_NUMERICHOST)
                     address = String(cString: hostname)
@@ -46,7 +39,20 @@ public enum SwiftIPConfig {
         return address
     }
 
-    public static func getNetmask() -> String? {
+    /// Returns Gateway IP. A gateway IP refers to a device on a network which sends local network traffic to other networks. For example: "192.168.1.1"
+    /// - Returns: Returns: String or nil
+    public static func getGatewayIP() -> String? {
+        var gatewayaddr = in_addr()
+        let result = get_gateway(&gatewayaddr.s_addr)
+        guard result >= 0 else {
+            return nil
+        }
+        return String(cString: inet_ntoa(gatewayaddr))
+    }
+
+    /// Returns Netmask. Netmasks (or subnet masks) are a shorthand for referring to ranges of consecutive IP addresses in the Internet Protocol. For example: "255.255.255.0"
+    /// - Returns: Returns: String or nil
+    public static func getNetmask(ethernet: String = "en0") -> String? {
         var netmask: String?
         var ifaddr: UnsafeMutablePointer<ifaddrs>? = nil
 
@@ -56,23 +62,28 @@ public enum SwiftIPConfig {
 
         var ptr = ifaddr
         while ptr != nil {
-            let name = String(utf8String: ptr!.pointee.ifa_name)
-            if (name == "en0"), let ifaFlags = ptr?.pointee.ifa_flags {
-                let flags = Int32(ifaFlags)
-                var addr = ptr?.pointee.ifa_addr.pointee
-                if (flags & (IFF_UP | IFF_RUNNING | IFF_LOOPBACK)) == (IFF_UP | IFF_RUNNING) {
-                    if addr?.sa_family == UInt8(AF_INET) || addr?.sa_family == UInt8(AF_INET6) {
-                        var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
-                        if getnameinfo(&addr!, socklen_t((addr?.sa_len)!), &hostname, socklen_t(hostname.count), nil, socklen_t(0), NI_NUMERICHOST) == 0 {
-                            var net = ptr?.pointee.ifa_netmask.pointee
-                            var netmaskName = [CChar](repeating: 0, count: Int(NI_MAXHOST))
-                            getnameinfo(&net!, socklen_t((net?.sa_len)!), &netmaskName, socklen_t(netmaskName.count), nil, socklen_t(0), NI_NUMERICHOST)
-                            netmask = String.init(validatingUTF8:netmaskName)
-                        }
+            defer { ptr = ptr?.pointee.ifa_next }
+
+            guard let interface = ptr?.pointee else {
+                return nil
+            }
+
+            let addrFamily = interface.ifa_addr.pointee.sa_family
+            if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
+                guard let ifa_name = interface.ifa_name else {
+                    return nil
+                }
+                let name: String = String(cString: ifa_name)
+                if name == ethernet {
+                    let flags = Int32(interface.ifa_flags)
+                    var addr = interface.ifa_addr.pointee
+                    if (flags & (IFF_UP | IFF_RUNNING | IFF_LOOPBACK)) == (IFF_UP | IFF_RUNNING) {
+                        var netmaskName = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                        getnameinfo(&addr, socklen_t(addr.sa_len), &netmaskName, socklen_t(netmaskName.count), nil, socklen_t(0), NI_NUMERICHOST)
+                        netmask = String.init(validatingUTF8:netmaskName)
                     }
                 }
             }
-            ptr = ptr?.pointee.ifa_next
         }
         freeifaddrs(ifaddr)
 
