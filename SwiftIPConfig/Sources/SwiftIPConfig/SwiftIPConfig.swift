5c2edfd033d08035f3bf79169b7a199473d31b11
Maksym Bilan <>
2022-06-25 18:36:17 +0300

    IP function

--- SwiftIPConfig/Sources/SwiftIPConfig/SwiftIPConfig.swift
+++ SwiftIPConfig/Sources/SwiftIPConfig/SwiftIPConfig.swift
@@ -11,4 +11,38 @@ public enum SwiftIPConfig {
         return String(cString: inet_ntoa(gatewayaddr))
     }
 
+    public static func getIP() -> String? {
+        var address: String?
+        var ifaddr: UnsafeMutablePointer<ifaddrs>?
+
+        guard getifaddrs(&ifaddr) == 0 else {
+            return nil
+        }
+
+        var ptr = ifaddr
+        while ptr != nil {
+            defer { ptr = ptr?.pointee.ifa_next }
+
+            guard let interface = ptr?.pointee else {
+                return nil
+            }
+
+            let addrFamily = interface.ifa_addr.pointee.sa_family
+            if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
+                guard let ifa_name = interface.ifa_name else {
+                    return nil
+                }
+                let name: String = String(cString: ifa_name)
+                if name == "en0" {
+                    var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                    getnameinfo(interface.ifa_addr, socklen_t((interface.ifa_addr.pointee.sa_len)), &hostname, socklen_t(hostname.count), nil, socklen_t(0), NI_NUMERICHOST)
+                    address = String(cString: hostname)
+                }
+            }
+        }
+        freeifaddrs(ifaddr)
+
+        return address
+    }
+
 }
