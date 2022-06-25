876080002197c32494e17e2c1426c2c2c1d8913c
Maksym Bilan <>
2022-06-25 18:34:17 +0300

    Gateway function

--- SwiftIPConfig/Package.swift
+++ SwiftIPConfig/Package.swift
@@ -5,22 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftIPConfig",
+    platforms: [
+        .iOS(.v9)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftIPConfig",
             targets: ["SwiftIPConfig"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "gateway",
+            path: "Sources/gateway"),
         .target(
             name: "SwiftIPConfig",
-            dependencies: []),
+            dependencies: ["gateway"],
+            path: "Sources/SwiftIPConfig"),
         .testTarget(
             name: "SwiftIPConfigTests",
             dependencies: ["SwiftIPConfig"]),
--- SwiftIPConfig/Sources/SwiftIPConfig/SwiftIPConfig.swift
+++ SwiftIPConfig/Sources/SwiftIPConfig/SwiftIPConfig.swift
@@ -1,6 +1,14 @@
-public struct SwiftIPConfig {
-    public private(set) var text = "Hello, World!"
+import gateway
 
-    public init() {
+public enum SwiftIPConfig {
+
+    public static func getGatewayIP() -> String? {
+        var gatewayaddr = in_addr()
+        let r = getdefaultgateway(&gatewayaddr.s_addr)
+        guard r >= 0 else {
+            return nil
+        }
+        return String(cString: inet_ntoa(gatewayaddr))
     }
+
 }
